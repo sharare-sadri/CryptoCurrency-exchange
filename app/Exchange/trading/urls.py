4b1e1157d047df7c65b04eece1e11d604ed2f4bb
Sharare <sharare@MacBookPro.Snapp.Net>
2025-10-06 15:10:26 +0330

    refactor(helm)/ refactor helm,readme,backup, cronjob

--- app/Exchange/trading/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from ...CryptoExchange import views
-
-urlpatterns = [
-    path("token/<slug:slug>", views.TokenDetailView.as_view(), name='token-detail'),
-]
--- app/Exchange/wallet/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models.wallet import Wallet
-from .models.token import Token
-from .models.history import History
-
-admin.site.register(Wallet)
-admin.site.register(Token)
-admin.site.register(History)
